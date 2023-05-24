bf36ef5c55134ce361dc70c1fd451fc83e6d9bbf
LeonSerrano272 <mattserrano272@gmail.com>
2023-05-24 08:18:26 -0600

    leon
    act

--- 2023_03_08_MayoryMenorQue/2023_03_08_MayoryMenorQue.cpp
+++ 2023_03_08_MayoryMenorQue/2023_03_08_MayoryMenorQue.cpp
@@ -5,24 +5,31 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
-int main() 
+int main()
 {
     int dato1 = 0;
-    int dato2 = 0;    
-    std::cout << "Este programa dara el numero mayor y menor" << std::endl; 
-    std::cout << "Inserte dato 1" << std::endl; 
+    int dato2 = 0;
+
+    std::cout << "Este programa dara el numero mayor y menor" << std::endl;
+    std::cout << "Inserte dato 1" << std::endl;
     std::cin >> dato1;
-    std::cout << "Inserte dato 2" << std::endl; 
+    std::cout << "Inserte dato 2" << std::endl;
     std::cin >> dato2;
+    if (dato1 == dato2)
+    {
+        std::cout << "Ambos datos son iguales" << std::endl;
+
+        exit(-1);
+    }
     if (dato1 > dato2)
     {
         std::cout << "El primer dato es mayor y el segundo menor" << std::endl;
 
-    exit(-1);
+        exit(-1);
     }
-    else (dato1 < dato2); 
+    else (dato1 < dato2);
     {
         std::cout << "El segundo dato es mayor y el primero menor" << std::endl;
     }
     exit(-1);
-}
+} 
---
+++ 2023_05_24_ArreglosDinamicos/2023_05_24_ArreglosDinamicos.cpp
@@ -0,0 +1,50 @@
+// 2023_05_24_ArreglosDinamicos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string> 
+int main()
+{
+    //Para crear areglos Dinamicos necesiotamos memoria no estatica
+    //Esto se logra con punteros
+    //Creamos la variable puntera y la inicializamos en NULL
+    std::string* titulos = NULL;
+    std::string* autores = NULL; 
+    //std:string[10]; Es para memoria estatica
+    int size; 
+    std::cout << "Cuantas peliculas quieres registrar?  " << std::endl;  
+    std::cin >> size; 
+    //Crear arreglos dinamicos
+    titulos = new std::string[size]; 
+    autores = new std::string[size];
+
+    //Ahora solicita la informacion 
+    for (int i = 0; i < size; i++)
+    {
+        if (i == 0)
+        {
+            std::cin.ignore();
+        }
+        std::cout << "TITULO  " << i << std::endl;
+       getline (std::cin, titulos[i]); 
+    }
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "AUTORES  " << i << std::endl;
+      getline (std::cin, autores[i]); 
+    }
+    //Imprime la informacion 
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "El titulo de la pelicula es: " << i + 1 << "- " << titulos[i] << std::endl;  
+        std::cout << "El autor de la pelicula es: " << i + 1 << "- " << autores[i] << std::endl; 
+    }
+
+    
+    //Recuerda siemrpe destruir la memoria dinamica
+    delete[] titulos; 
+    delete[] autores; 
+    titulos = NULL; 
+    autores = NULL; 
+}
+
