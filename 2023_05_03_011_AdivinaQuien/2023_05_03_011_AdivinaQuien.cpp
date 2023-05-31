14e030d4a8755c59a9b70b72fb221c33371f490d
LeonSerrano272 <mattserrano272@gmail.com>
2023-05-31 08:50:01 -0600

    Leon
    Act

--- 2023_05_03_011_AdivinaQuien/2023_05_03_011_AdivinaQuien.cpp
+++ 2023_05_03_011_AdivinaQuien/2023_05_03_011_AdivinaQuien.cpp
@@ -15,107 +15,84 @@ int main()
         std::cout << "El deporte que piensas se juega con balon?? " << std::endl;
         std::cin >> aux;
 
-        if (aux == true) // Se juega con balon?
+        if (aux == true) //equipo de 5
         {
             std::cout << "El equipo es de 5 jugadores por equipo?" << std::endl;
             std::cin >> aux;
         }
-        else //No se juega con balon
+        if (aux == true) //Balon basket
         {
-            std::cout << "Se practica en una pista??" << std::endl;
+            std::cout << "El balón es pesado en comparacion de otros?" << std::endl;
             std::cin >> aux;
-            if (aux == true)// Se practica en una pista?
-            {
-                std::cout << "Es individual?? " << std::endl;
-                std::cin >> aux;
-                if (aux == true) // Se practica en estadios?
-                {
-                    std::cout << "Se practica en estadios??" << std::endl;
-                    std::cin >> aux;
-                }
-                if (aux == true) // Deporte olimpico?
-                {
-                    std::cout << "Es deporte Olimpico??" << std::endl;
-                    std::cin >> aux;
-                }
-                if (aux == true) // De tierra o de agua
-                {
-                    std::cout << "Deporte de tierra??" << std::endl;
-                    std::cin >> aux;
-                }
-                else // De tierra o de agua
-                {
-                    std::cout << "Deporte de agua??" << std::endl;
-                    std::cin >> aux;
-
-                    if (aux == true) // De tierra o de agua
-                    {
-                        std::cout << "Se necesita aguantar la respiracion??" << std::endl;
-                        std::cin >> aux;
-                    }
-                if (aux == true)  //Se practica en pista de agua
-                {
-                    std::cout << " Tu deporte es natacion" << std::endl;
-                    std::cin >> aux;
-                }
-                }
-                return 3;
-
-                if (aux == true) // // Areas 
-                {
-                    std::cout << "Tiene muchas areas derivadas ese deporte?? " << std::endl;
-                    std::cin >> aux; 
-                }
-                if (aux == true) // Tu deporte es atletismo
-                {
-                    std::cout << "Tu deporte es atletismo" << std::endl;
-                }
-                return 3;
-            }
-                if (aux == true) // Se juega de 5 jugadores en un equipo?
-                {
-                    std::cout << "El balon es pesado en comparacion de otros?? " << std::endl;
-                    std::cin >> aux;
-                    if (aux == true) // Es pesado el balon?
-                    {
-                        std::cout << "El objetivo es meter el balon en una canasta??" << std::endl;
-                        std::cin >> aux;
-                    }
-                    if (aux == true) // Meter balon en canasta?
-                    {
-                        std::cout << "Tu deporte es basquetbol :D" << std::endl;
-                    }
-                }
-                else //Futbol 
-                {
-                    std::cout << "Tu deporte es de 11 jugadores por equipo?" << std::endl;
-                    std::cin >> aux;
-                }
-                if (aux == true) // Equipos de 11
-                {
-                    std::cout << "El objetivo es meter el balon en una porteria??" << std::endl;
-                    std::cin >> aux;
-                }
-                if (aux == true) // Balon en porteria 
-                {
-                    std::cout << "Tu deporte es futbol" << std::endl;
-                }
-                else // Voleyball
-                {
-                    std::cout << "Tu deporte es de 6 jugadores por equipo?" << std::endl;
-                    std::cin >> aux;
-                }
-                if (aux == true) //Deporte de 6 jugadores por equipo
-                {
-                    std::cout << "Tu deporte tiene una red en medio del espacio de juego?" << std::endl;
-                    std::cin >> aux;
-                }
-                if (aux == true) // Red en medio del juego
-                {
-                    std::cout << "Tu deporte es voleyball" << std::endl;
-                    std::cin >> aux;
-                }
-            }
-
         }
+        if (aux == true) //Canasta
+        {
+            std::cout << "El balon entra en una canasta alta?" << std::endl;
+            std::cin >> aux;
+        }
+        if (aux == true) //Resultado 1
+        {
+            std::cout << "Tu deporte es baloncesto" << std::endl;
+            system("PAUSE");
+
+            return 0;
+        }
+        else (aux == false); //Si no se juega con 5 jugadores por equipo
+        {
+            std::cout << "El equipo es de 6 jugadores?" << std::endl;
+            std::cin >> aux;
+        }
+        if (aux == true) //Balon de volley
+        {
+            std::cout << "El balón es muy ligero?" << std::endl;
+            std::cin >> aux;
+        }
+        if (aux == true) //Malla de volley
+        {
+            std::cout << "El balón se pasa a traves de una red?" << std::endl; 
+            std::cin >> aux; 
+        }
+        if (aux == true) //Punto en volleyball
+        {
+            std::cout << "Si el balon toca el piso es punto para el que logro hacer que el otro equipo se le cayera?" << std::endl; 
+            std::cin >> aux; 
+        }
+        if (aux == true) //Resultado 2
+        {
+            std::cout << "Tu deporte es Volleyball" << std::endl;
+            system("PAUSE");
+
+            return 0;
+        }
+        else (aux == false);  //Si no se juega de 6 jugadores por equipo
+        {
+            std::cout << "El equipo es de 11 jugadores por equipo?" << std::endl;  
+            std::cin >> aux; 
+        }
+        if (aux == true) //Balon de futbol
+        {
+            std::cout << "El balon es duro pero no tan grande como uno de baloncesto?" << std::endl; 
+            std::cin >> aux; 
+        }
+        if (aux == true) //Cancha de futbol
+        {
+            std::cout << "Se juega en una cancha con pasto?" << std::endl; 
+            std::cin >> aux;
+        }
+        if (aux == true) //Porteria de futbol
+        {
+            std::cout << "Se tiene que meter el balón en una porteria?" << std::endl; 
+            std::cin >> aux; 
+        }
+        if (aux == true) //Resultado 2
+        {
+            std::cout << "Tu deporte es fubol" << std::endl;  
+            system("PAUSE");
+
+            return 0;
+        }
+
+
+
     }
+}
